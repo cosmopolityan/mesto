de564abd60ab8c94bfbed80d91db4e8869fe4144
Mark <cosmopolityan@yandex.ru>
2022-01-06 15:26:42 +0300

    feat: Array initialCards

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Камчатка',
+    link: '../images/kamchatka.jpg'
+  },
+  {
+    name: 'Мурманск',
+    link: '../images/murmansk.jpg'
+  },
+  {
+    name: 'Озеро Байкал',
+    link: '../images/baikal-lake.jpg'
+  },
+  {
+    name: 'Алтай',
+    link: '../images/altay.jpg'
+  },
+  {
+    name: 'Приморский край',
+    link: '../images/primorskiy-kray.jpg'
+  },
+  {
+    name: 'Башкирия',
+    link: '../images/poperechnaya-gora.jpg'
+  }
+];
+
 let profileElement = document.querySelector('.profile');
 // Объявленная переменная = результат поиска секции .profile во всем документе.
 
