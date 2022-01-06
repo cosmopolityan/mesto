c961262f5846a8c1ac0e3a05750911b40f9efef1
Mark <cosmopolityan@yandex.ru>
2022-01-06 15:04:07 +0300

    feat: продублировал все функции, чтобы открывался 2-ой попап

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 let profileElement = document.querySelector('.profile');
 // Объявленная переменная = результат поиска секции .profile во всем документе.
 
-
 let editButton = profileElement.querySelector('.profile__edit-button');
 // Объявленная переменная = результат поиска кнопки .profile__edit-button в секции .profile.
 let addButton = profileElement.querySelector('.profile__add-button')
@@ -13,39 +12,58 @@ let nameElement = profileElement.querySelector('.profile__name');
 let jobElement = profileElement.querySelector('.profile__description');
 // Объявленная переменная = результат поиска поля .profile__description в секции .profile.
 
-let popupElement = document.querySelector('.popup');
-// Объявленная переменная = результат поиска секции .popup во всем документе.
-const popupElementOpen = 'popup_opened';
+let popup1Element = document.querySelector('#pop1');
+// Объявленная переменная = результат поиска секции с id #pop1 во всем документе.
+
+//06.01.22
+let popup2Element = document.querySelector('#pop2');
+// Объявленная переменная = результат поиска секции с id #pop2 во всем документе.
+
+const popup1ElementOpen = 'popup_opened';
 // Объявленная константа со значением класса, чтобы в дальнейшем передать ее как аргумент в функцию.
 
-let closeButton = popupElement.querySelector('.popup__close-button');
+const popup2ElementOpen = 'popup_opened';
+
+let close1Button = popup1Element.querySelector('.popup__close-button');
 // Объявленная переменная = результат поиска кнопки .popup__close-button в секции .popup.
-let formElement = popupElement.querySelector('.popup__form');
+let form1Element = popup1Element.querySelector('.popup__form');
 // Объявленная переменная = результат поиска формы .popup__form в секции .popup.
 
-let nameInput = formElement.querySelector('.popup__input[name="name"]');
+let close2Button = popup2Element.querySelector('.popup__close-button');
+// Объявленная переменная = результат поиска кнопки .popup__close-button в секции .popup.
+let form2Element = popup2Element.querySelector('.popup__form');
+// Объявленная переменная = результат поиска формы .popup__form в секции .popup.
+
+let nameInput = form1Element.querySelector('.popup__input[name="name"]');
 // Объявленная переменная = результат поиска инпута .popup__input с id = name в форме .popup__form.
-let jobInput = formElement.querySelector('.popup__input[name="job"]');
+let jobInput = form1Element.querySelector('.popup__input[name="job"]');
 // Объявленная переменная = результат поиска инпута .popup__input с id = job в форме .popup__form.
 
-let titleInput = formElement.querySelector('.popup__input[name="title"]');
+// let titleInput = formElement.querySelector('.popup__input[name="title"]');
 // Объявленная переменная = результат поиска инпута .popup__input с id = title в форме .popup__form.
-let linkInput = formElement.querySelector('.popup__input[name="link"]');
+// let linkInput = formElement.querySelector('.popup__input[name="link"]');
 // Объявленная переменная = результат поиска инпута .popup__input с id = link в форме .popup__form.
 
-function popupToggle() {
-  popupElement.classList.toggle(popupElementOpen);
+function popup1Toggle() {
+  popup1Element.classList.toggle(popup1ElementOpen);
 }
 // Функция добавления/удаления класса .popup_opened элементу .popup.
 
-function popupOpen() {
+function popup2Toggle() {
+  popup2Element.classList.toggle(popup2ElementOpen);
+}
+
+function popup1Open() {
   nameInput.value = nameElement.textContent;
   jobInput.value = jobElement.textContent;
 // Без этих 2 строк, попап открывается, но в полях нет значений из DOM, только информация из placeholder'а.
-  popupToggle(); // '.popup' => '.popup popup_opened'
+  popup1Toggle(); // '.popup' => '.popup popup_opened'
 }
 // Функция присваивает полям "вытянутые" из DOM значения в соотв. полях.
 
+function popup2Open() {
+  popup2Toggle(); // '.popup' => '.popup popup_opened'
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -53,15 +71,17 @@ function formSubmitHandler(evt) {
   nameElement.textContent = nameInput.value;
   jobElement.textContent = jobInput.value;
 
-  popupToggle(); // '.popup popup_opened' => '.popup'
+  popup1Toggle(); // '.popup popup_opened' => '.popup'
 }
 // Функция "перезаписывает" имеющиеся значения на новые, введенные пользователем.
 
-editButton.addEventListener('click', popupOpen);
+editButton.addEventListener('click', popup1Open);
 // добавленный ивентлиссенер по клику вызывает функцию popupOpen => попап открывается.
-addButton.addEventListener('click', popupOpen);
-// добавленный ивентлиссенер по клику вызывает функцию popupOpen => попап открывается.
-closeButton.addEventListener('click', popupToggle);
+addButton.addEventListener('click', popup2Open);
+// добавленный ивентлиссенер по клику вызывает функцию popupOpen => попап открывается (Открывается тот же самый попап, что и в editButton)
+close1Button.addEventListener('click', popup1Toggle);
 // добавленный ивентлиссенер по клику вызывает функцию popupToggle -> '.popup popup_opened' => '.popup'.
-formElement.addEventListener('submit', formSubmitHandler);
+close2Button.addEventListener('click', popup2Toggle);
+// добавленный ивентлиссенер по клику вызывает функцию popupToggle -> '.popup popup_opened' => '.popup'.
+form1Element.addEventListener('submit', formSubmitHandler);
 // добавленный ивентлиссенер по клику вызывает функцию formSubmitHandler -> отменяет стандартную отправку формы, устанавливает введенные пользователем значения в соотв. поля, перезаписывает их в DOM после нажатия кнопки 'Сохранить' [submit].
