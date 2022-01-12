1071717ffebcde6919f5d5908576729737cbd211
Mark <cosmopolityan@yandex.ru>
2022-01-12 22:19:10 +0300

    refactor: .js (let->const, img alt)

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-// Здравствуйте, Геннадий!
-// Большое спасибо за такое оперативное и детальное ревью!
-// Постарался все ошибки исправить, насколько понял их. Над моментами "Можно лучше" буду сознательно работать уже потом, пока что первостепенная и самая важная для меня цель - до 16.01.22 закрыть 5 и 6 спринт.
+// changelog 2022-01-12T22:12
+// 1. все переменные let исправлены на const;
+// 2. у всех картинок alt;
 
 // Начальный массив карточек-объектов
 const initialCards = [
@@ -32,24 +32,30 @@ const initialCards = [
 
 //
 
-const photosList = document.querySelector('.elements__list'); // исправлено с let
+const photosList = document.querySelector('.elements__list');
 
-let popupPhotos = document.querySelector('.popup-photo');
-let popupPhotosImage = popupPhotos.querySelector('.popup-photo__image');
-let popupPhotosCaption = popupPhotos.querySelector('.popup-photo__caption');
-let popupPhotosCloseButton = popupPhotos.querySelector('.popup__close-button');
+const popupPhotos = document.querySelector('.popup-photo');
+const popupPhotosImage = popupPhotos.querySelector('.popup-photo__image');
+const popupPhotosCaption = popupPhotos.querySelector('.popup-photo__caption');
+const popupPhotosCloseButton = popupPhotos.querySelector('.popup__close-button');
 
 function addPhotosElement(name, link, where) {
   const photosElement = document.querySelector('#element-template').content;
   const photosCard = photosElement.cloneNode(true);
   const photosImage = photosCard.querySelector('.element__image');
   const photosLikeButton = photosCard.querySelector('.element__like-button');
-  const photosDeleteButton = photosCard.querySelector('.element__trash-button');
+  const photosDeconsteButton = photosCard.querySelector('.element__trash-button');
   photosImage.src = link;
+  photosImage.alt = name;
   photosCard.querySelector('.element__title').textContent = name;
-  photosImage.addEventListener('click', openPhoto);
+  photosImage.addEventListener('click', openPhoto); //
+  // Будет лучше, если функция открытия модального окна с картинкой будет принимать название карточки и ссылку вместо события evt.
+  // Таким образом, Вам не придется брать данные из события, Вы сможете сразу устанавливать необходимые значения для картинки и заголовка.
+  // Для того чтобы передавать аргументы внутри колбэка, Вам следует воспользоваться стрелочной функцией:
+  // photosImage.addEventListener('click', () => openPhoto(item));
+  // Пока что не понял что openPhoto должен принимать в качестве аргумента (-ов). Все логически возможные варианты выдают в консоли одну и ту же ошибку (not defined). Или нужно переписывать еще саму функцию openPhoto, пока что непонятно.
   photosLikeButton.addEventListener('click', likePhoto);
-  photosDeleteButton.addEventListener('click', deleteButton);
+  photosDeconsteButton.addEventListener('click', deconsteButton);
   where === 'append' ? photosList.append(photosCard) : photosList.prepend(photosCard);
 }
 
@@ -65,7 +71,7 @@ initializePhotos(initialCards);
 
 //
 
-let popupAdd = document.querySelector('#card_popup');
+const popupAdd = document.querySelector('#card_popup');
 
 // функция открытия любого попапа
 
@@ -87,8 +93,8 @@ function emptyInputValue(...inputs) {
   inputs.map(elem => elem.value = '');
 }
 
-let inputPhotoName = popupAdd.querySelector('#photo-name');
-let inputPhotoLink = popupAdd.querySelector('#photo-link');
+const inputPhotoName = popupAdd.querySelector('#photo-name');
+const inputPhotoLink = popupAdd.querySelector('#photo-link');
 
 function addCard(evt) {
   evt.preventDefault();
@@ -101,38 +107,38 @@ function likePhoto(evt) {
   evt.target.classList.toggle('element__like-button_active');
 }
 
-function deleteButton(evt) {
+function deconsteButton(evt) {
   evt.target.closest('.element').remove();
 }
 
-let profileElement = document.querySelector('.profile');
+const profileElement = document.querySelector('.profile');
 // Объявленная переменная = результат поиска секции .profile во всем документе.
 
-let editButton = profileElement.querySelector('.profile__edit-button');
+const editButton = profileElement.querySelector('.profile__edit-button');
 // Объявленная переменная = результат поиска кнопки .profile__edit-button в секции .profile.
-let addButton = profileElement.querySelector('.profile__add-button');
+const addButton = profileElement.querySelector('.profile__add-button');
 // Объявленная переменная = результат поиска кнопки .profile__add-button в секции .profile.
 
-let nameElement = profileElement.querySelector('.profile__name');
+const nameElement = profileElement.querySelector('.profile__name');
 // Объявленная переменная = результат поиска поля .profile__name в секции .profile.
 
-let jobElement = profileElement.querySelector('.profile__description');
+const jobElement = profileElement.querySelector('.profile__description');
 // Объявленная переменная = результат поиска поля .profile__description в секции .profile.
 
-let profilePopup = document.querySelector('#profile_popup');
+const profilePopup = document.querySelector('#profile_popup');
 // Объявленная переменная = результат поиска секции с id #profile_popup во всем документе.
 
-let profilePopupCloseButton = profilePopup.querySelector('.popup__close-button');
+const profilePopupCloseButton = profilePopup.querySelector('.popup__close-button');
 // Объявленная переменная = результат поиска кнопки .popup__close-button в секции .popup.
-let editProfileForm = profilePopup.querySelector('.popup__form');
+const editProfileForm = profilePopup.querySelector('.popup__form');
 // Объявленная переменная = результат поиска формы .popup__form в секции .popup.
 
-let popupAddCloseButton = popupAdd.querySelector('.popup__close-button');
+const popupAddCloseButton = popupAdd.querySelector('.popup__close-button');
 // Объявленная переменная = результат поиска кнопки .popup__close-button в секции .popup.
 
-let nameInput = editProfileForm.querySelector('.popup__input[name="name"]');
+const nameInput = editProfileForm.querySelector('.popup__input[name="name"]');
 // Объявленная переменная = результат поиска инпута .popup__input с id = name в форме .popup__form.
-let jobInput = editProfileForm.querySelector('.popup__input[name="job"]');
+const jobInput = editProfileForm.querySelector('.popup__input[name="job"]');
 // Объявленная переменная = результат поиска инпута .popup__input с id = job в форме .popup__form.
 
 function openProfilePopup() {
@@ -165,10 +171,11 @@ function changeProfileData(evt) {
 }
 
 function openPhoto(evt) {
-  let box = evt.path[1];
-  let img = box.querySelector('.element__image');
-  let caption = box.querySelector('.element__title');
+  const box = evt.path[1];
+  const img = box.querySelector('.element__image');
+  const caption = box.querySelector('.element__title');
   popupPhotosImage.src = img.src;
+  popupPhotosImage.alt = img.alt;
   popupPhotosCaption.textContent = caption.textContent;
 
   openPopup(popupPhotos);
