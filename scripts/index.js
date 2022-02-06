8181bff8356e8c5a347096f6af721b560d43b654
Mark <cosmopolityan@yandex.ru>
2022-02-06 18:45:10 +0300

    fix: edit-profile-save-button

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 import { enableValidation } from './enableValidation.js';
 import { initialCards } from './initialCards.js';
 import { photosList, popupAdd, inputPhotoName, inputPhotoLink, editForm, addForm, submitEditButton, submitAddButton, editButton, addButton, nameElement, jobElement, profilePopup, profilePopupCloseButton, popupAddCloseButton, nameInput, jobInput } from './consts.js';
-import { openPopup, closePopup } from './utils.js'
+import { openPopup, closePopup } from './utils.js';
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
 
@@ -20,19 +20,19 @@ const openAddPopup = () => {
   inputPhotoName.value = '';
   inputPhotoLink.value = '';
 
-  addValidator.setDisableButton(submitAddButton)
+  addValidator.setDisableButton(submitAddButton);
   addValidator.clearErrorElements();
   openPopup(popupAdd);
-}
+};
 
 const openProfilePopup = () => {
   nameInput.value = nameElement.textContent;
   jobInput.value = jobElement.textContent;
 
-  editValidator.setAbleButton(submitEditButton)
+  editValidator.setAbleButton(submitEditButton);
   editValidator.clearErrorElements();
   openPopup(profilePopup);
-}
+};
 
 const changeProfileData = (evt) => {
   evt.preventDefault();
@@ -41,14 +41,14 @@ const changeProfileData = (evt) => {
   jobElement.textContent = jobInput.value;
 
   closePopup(profilePopup);
-}
+};
 
 // **************
 
 const createCard = (obj) => {
   const card = new Card(obj);
   return card.addPhotosElement();
-}
+};
 // Начальные карточки
 initialCards.forEach((item) => {
   const cardElement = createCard(item);
@@ -59,10 +59,10 @@ initialCards.forEach((item) => {
 const cardSubmit = (evt) => {
   evt.preventDefault();
   const cardElement = createCard({name: inputPhotoName.value, link: inputPhotoLink.value});
-  photosList.prepend(cardElement)
+  photosList.prepend(cardElement);
   closePopup(popupAdd);
   addForm.reset();
-}
+};
 
 // ***************************************************************
 
@@ -80,11 +80,11 @@ addForm.addEventListener('submit', (evt) => {
 
 const closeProfilePopup = () => {//
   closePopup(profilePopup);
-}
+};
 
 const closeAddPopup = () => {//
   closePopup(popupAdd);
-}
+};
 
 // EventListener'ы
 
@@ -96,5 +96,4 @@ profilePopupCloseButton.addEventListener('click', closeProfilePopup);
 // добавленный ивентлиссенер по клику вызывает функцию popupToggle -> '.popup popup_opened' => '.popup'.
 popupAddCloseButton.addEventListener('click', closeAddPopup);
 // добавленный ивентлиссенер по клику вызывает функцию popupToggle -> '.popup popup_opened' => '.popup'.
-editForm.addEventListener('submit', changeProfileData);
-// добавленный ивентлиссенер по клику вызывает функцию changeProfileData -> отменяет стандартную отправку формы, устанавливает введенные пользователем значения в соотв. поля, перезаписывает их в DOM после нажатия кнопки 'Сохранить' [submit].
+
